0c537f5ac635ae7779a4773d52223a36084f7af0
dandrade1206 <dandrade1206@gmail.com>
2017-06-12 16:14:39 -0500

    added Rider Detail

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import NewDrive from './NewDrive.js';
 import Login from './Login';
+import RiderDetail from './RiderDetail';
 
 class App extends Component {
   render() {
     return (
       <div className="row">
         <div className="App columns small-12 medium-6 large-4 xlarge-3 small-centered">
-         <NewDrive />
-         <Login />
+          <Login />
+          <NewDrive />
+          <RiderDetail />
         </div>
       </div>
     );
---
+++ src/RiderDetail.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class RiderDetail extends Component {
+  render() {
+    return (    
+            <div>
+                <h2>3:00pm ride Downtown</h2>
+                <br/>
+                <h2>with</h2>
+                <br/>
+                <h2> <a>Daniel Andrade</a> </h2>
+                <br/>
+                <textarea readonly> description of driver </textarea>
+                <button> Cancel </button>
+            </div>
+    );
+  }
+}
+
+export default RiderDetail;
