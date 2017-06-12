d235bc36e0d2e08dc45bcc6632a35d991a5540a8
Zach Willard <zacharywillard@northwesternmutual.com>
2017-06-12 14:52:25 -0500

    Added UI Toolkit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="row">
+        <div className="App columns small-12 medium-6 large-4 xlarge-3 small-centered">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
