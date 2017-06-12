2ff9b0b37454ca1343ff05c17bf481ffc1614b6b
Zach Willard <zacharywillard@northwesternmutual.com>
2017-06-12 15:20:26 -0500

    created new ride components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NewDrive from './NewDrive.js';
 
 class App extends Component {
   render() {
     return (
       <div className="row">
         <div className="App columns small-12 medium-6 large-4 xlarge-3 small-centered">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+
+          <NewDrive />
         </div>
       </div>
     );
