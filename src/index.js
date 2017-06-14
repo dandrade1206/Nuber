15ba19faba278c6b3095c823ce6cb3dff8f0a85c
dandrade1206 <dandrade1206@gmail.com>
2017-06-14 14:24:10 -0500

    adding routes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import rootReducer from './reducers/rootReducer'
 
 import { Provider }  from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter as Router, Route} from 'react-router';
 
 let store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <App/>
     </Provider>, 
     document.getElementById('root')
 );
